d90a448894de43eafd247db3007e42f3c2f6bb68
David Shapiro <DavidS@dlsa.com>
2017-09-07 15:18:45 -0500

    Finished Translated filename stripper.

--- src/StripTranslatedFileNames.groovy
+++ src/StripTranslatedFileNames.groovy
@@ -1,14 +1,32 @@
 import FileManagement.FileChooser
+import Logging.Dates
+import Logging.Log
 
 class StripTranslatedFileNames {
 
     static stripTranslatedFromFileNames(changeLibrary) {
-        println changeLibrary
+        def list = new File(changeLibrary)
+        list.eachFileMatch(~/.*.translated/) {
+            def newName = it.getPath().replaceAll(~/.translated/, "")
+            def result = it.renameTo(new File(newName))
+            if (result)
+                Log.writeLine("changed name of $it \r\nto: $newName")
+            else
+                Log.writeLine("exceptions", "could not change name of $it \r\nto: $newName")
+        }
+    }
+
+    static openLogs(fp) {
+        Log.open(fp + "log-rename-translated-files.txt")
+        Log.writeLine("Running on: " + Dates.currentDateAndTime())
+        Log.open("exceptions", fp + "log-rename-exceptions.txt")
+        Log.writeLine("exceptions", "Running on: " + Dates.currentDateAndTime())
     }
 
     static main(args) {
         def fp = "C:\\\\Users\\\\s0041664\\\\Documents\\\\Projects\\\\DMT-DE\\\\Translations\\\\"
         // pick library folder (translated files)
+        openLogs(fp)
         def changeLibrary = FileChooser.chooseFile("Select Directory Folder for Name Changes", fp, FileChooser.selectMode.DIRECTORIES)
         if (changeLibrary != null) {
             stripTranslatedFromFileNames(changeLibrary)
