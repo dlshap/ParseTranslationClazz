da02acd30af5010e2335a1bea54e57ab2cb9f1cd
dlshap <DavidS@dlsa.com>
2018-12-04 10:37:12 -0600

    cleaned up IgnorePropertyList code to use 1 regex instead of 2

--- src/translations/IgnorePropertyList.groovy
+++ src/translations/IgnorePropertyList.groovy
@@ -4,19 +4,30 @@ import filemanagement.TextFile
 
 class IgnorePropertyList {
 
-    private ignorePropertyListFile
+    private ignorePropertyMsgFile
+    private ignorePropertyMsgList
     def ignorePropertyList
 
     def IgnorePropertyList(filename) {
-        ignorePropertyListFile = new TextFile(filename)
-        loadIgnorePropertyList()
+        ignorePropertyMsgFile = new TextFile(filename)
+        buildIgnorePropertyList()
     }
 
-    private def loadIgnorePropertyList() {
-        def ignoreList = ignorePropertyListFile.getText()
-        if (ignoreList != null) {
-            def matchingLines = ignoreList.findAll(/[^#]Property.+?'.*?'.*/).collect()
-            ignorePropertyList = matchingLines.collect { (it =~ /Property.+?'(.*?)'.*/)[0][1] }
+    private def buildIgnorePropertyList() {
+        loadTextFromIgnoreMsgFile()
+        parseIgnorePropertyListFromMsgText()
+    }
+
+    private loadTextFromIgnoreMsgFile() {
+        ignorePropertyMsgList = ignorePropertyMsgFile.getText()
+    }
+
+    private parseIgnorePropertyListFromMsgText() {
+        // Example: get property name: xxx from msg like: "Property 'xxx' not found..."
+        // Ignore lines beginning with "#"
+        if (ignorePropertyMsgList != null) {
+            def ignoreMsgList = ignorePropertyMsgList =~ /[^#]Property.+?'(.*?)'.*/
+            ignorePropertyList = ignoreMsgList.collect { it[1] }
         }
     }
 
