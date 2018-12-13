2aba7ba98adfae9dd5fdf46aba3a59a885bf92b9
dlshap <DavidS@dlsa.com>
2018-12-13 14:38:43 -0600

    Only translate for "Translated" keys

--- src/UpdateDMTClassFactories.groovy
+++ src/UpdateDMTClassFactories.groovy
@@ -223,11 +223,13 @@ class UpdateDMTClassFactories {
         libraryQuestionTranslators.eachWithIndex { it, i ->
             // get field name from translator
             def translationKey = it.getValue("excelColumnName")
-            // get translation value from translation (keyfile)
-            def translationValue = translation.get(translationKey)
-            // translate it if there is a match...leave alone if not
-            if (translationValue != "") {
-                nextFactoryBlock = it.translate(nextFactoryBlock, translationValue, bomFieldName)
+            if (translationKey.toLowerCase().contains("translated")) {
+                // get translation value from translation (keyfile)
+                def translationValue = translation.get(translationKey)
+                // translate it if there is a match...leave alone if not
+                if (translationValue != "") {
+                    nextFactoryBlock = it.translate(nextFactoryBlock, translationValue, bomFieldName)
+                }
             }
         }
         nextFactoryBlock
