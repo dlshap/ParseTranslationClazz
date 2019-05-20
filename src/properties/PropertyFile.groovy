dc821422c0473d218e32bbf51b8b58184ca03d90
dlshap <DavidS@dlsa.com>
2019-05-20 14:25:15 -0500

    Fix date formatting, and add new directory for properties files

--- src/properties/PropertyFile.groovy
+++ src/properties/PropertyFile.groovy
@@ -1,15 +1,11 @@
 package properties
 
-import filemanagement.FileChooser
 import filemanagement.LineFile
-import i18n.Messages
 import translations.TranslationProperties
 
 import static filemanagement.BaseFile.CreateFlag.CREATE
 
 class PropertyFile extends LineFile {
-
-    static final PROPERTY_FILE_PROMPT = "prompt.for.message.properties.file.for"
 
     PropertyFile() {
     }
@@ -25,32 +21,10 @@ class PropertyFile extends LineFile {
         filePath + "\\PropertyFilesTranslated\\" + fileName + ".translated"
     }
 
-    static openPropertyFileForComponentUsingChooser(componentName, componentPath) {
-        PropertyFile propertyFile = new PropertyFile()
-        def prompt = Messages.getString(PROPERTY_FILE_PROMPT, componentName)
-        propertyFile.openFile(FileChooser.chooseFile(prompt, componentPath))
-        propertyFile
-    }
-
     static openPropertyFileFromFileName(String fileName) {
         PropertyFile propertyFile = new PropertyFile()
         propertyFile.openFile(fileName)
         propertyFile
-    }
-
-    def getTranslatedOutputFileName() {
-        def parentDir = new File(getDirPath()).getParent()
-        def propertyOutFileName = outputFileName(parentDir, fileName)
-        propertyOutFileName
-    }
-
-    def openTranslatedOutputFile() {
-        LineFile translatedFile
-        if (file != null) {
-            def propertyOutFileName = this.getTranslatedOutputFileName()
-            translatedFile = new LineFile(propertyOutFileName, CREATE)
-        }
-        translatedFile
     }
 
     def getTranslationProperties() {
